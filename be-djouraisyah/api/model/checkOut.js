1e1f93674676780d9d21e313bfbb7a8a3458891a
aisyah213110006 <aisyahfajrisyahnur566@gmail.com>
2024-07-10 18:27:13 +0700

    adding file from vscode

---
+++ be-djouraisyah/api/model/checkOut.js
@@ -0,0 +1,41 @@
+// const dbConection = require('./dbConnection');
+// const sequelize = require('sequelize');
+
+// // nama tabel database
+// const checkOut = dbConection.define("checkout", {
+//     id: {
+//         type: sequelize.DataTypes.INTEGER,
+//         primaryKey: true
+//     },
+//     product_name: {
+//         type: sequelize.DataTypes.STRING
+//     },
+//     quantity: {
+//         type: sequelize.DataTypes.STRING
+//     },
+//     price: {
+//         type: sequelize.DataTypes.STRING
+//     },
+//     name: {
+//         type: sequelize.DataTypes.STRING
+//     },
+//     quantity: {
+//         type: sequelize.DataTypes.STRING
+//     },
+//     phone_number: {
+//         type: sequelize.DataTypes.STRING
+//     },
+//     address: {
+//         type: sequelize.DataTypes.TEXT
+//     },
+//     payment_method: {
+//         type: sequelize.DataTypes.STRING
+//     },
+//     shipping_method: {
+//         type: sequelize.DataTypes.STRING
+//     }
+// }, {
+//     freezeTableName: true,
+//     timestamps: false
+// });
+// module.exports = checkOut;
